a52d03e0e0635c0c237a076a1ffed69e345d748a
Adrian Dabrowski <adi04@o2.pl>
2022-12-21 13:33:48 +0100

    name

--- terraform/azure/networking.tf
+++ terraform/azure/networking.tf
@@ -126,6 +126,7 @@ resource azurerm_network_watcher "network_watcher" {
 
 resource azurerm_network_watcher_flow_log "flow_log" {
   enabled                   = false
+  name                 = "example-log"
   network_security_group_id = azurerm_network_security_group.bad_sg.id
   network_watcher_name      = azurerm_network_watcher.network_watcher.name
   resource_group_name       = azurerm_resource_group.example.name
